8e14d3740086da4f238747f971e6839ee628abca
aeiou205 <106573426+aeiou205@users.noreply.github.com>
2022-09-13 11:28:34 -0500

    Update Herencia.py

--- Herencia.py
+++ Herencia.py
@@ -60,7 +60,7 @@ class ElectricCar(Car):
         """Initialize attributes of the parent class
         them initialize attributes specific to an electric car"""
         super().__init__(make, model, year) #la funcion super es una fucnion especial que le permite llamar un metodo de la clase padre
-        self.battery_size = Battery()         #le dice a python que llame a init a esta seccion
+        self.battery = Battery()         #le dice a python que llame a init a esta seccion
 
 
     def describe_battery(self):
@@ -93,11 +93,10 @@ def main():
 
     my_tesla = ElectricCar('tesla', 'model s', 2019)
     print(my_tesla.get_descriptive_name())
-    my_tesla.battery_size.describe_battery()
+    my_tesla.battery.describe_battery()
 
     gas.get_range()
 
 if __name__ == "__main__":
     main()
-
 
